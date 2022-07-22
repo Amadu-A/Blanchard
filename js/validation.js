51157fa6cd65fc0ffe654fa66d22f96f7ae8cde8
Мейн Иван Сергеевич <Buh02@MV-SERVICE.MVSO>
2022-07-22 11:09:18 +0300

    valodation s rules not work

--- js/validation.js
+++ js/validation.js
@@ -66,15 +66,15 @@
       },
       name: {
         required: true,
-        minLength: 4,
+        minLength: 3,
         maxLength: 30,
 
       },
       messages: {
         name: {
           required: "Вы не ввели имя",
-          minlength: "Please include your complete name",
-          maxlength: "Name is too long. Please abbreviate.",
+          minlength: "Имя должно иметь более 3 символов",
+          maxlength: "Имя должно быть менее 30 символов",
         },
 
         tel: {
@@ -84,69 +84,6 @@
       },
     },
 
-    // {
-    //   required: true,
-    //   rule: 'minLength',
-    //   value: 3,
-    //   errorMessage: 'Имя должно содержать хотя бы 3 буквы'
-    // }
   }, '.thanks-popup', 'send goal');
 
-  // const validation = new window.JustValidate('.contacts__form', {
-  //   errorFieldCssClass: 'is-invalid',
-  //   errorFieldStyle: {
-  //     border: '1px solid var(--error)',
-  //   },
-  //   errorLabelCssClass: 'is-label-invalid',
-  //   errorLabelStyle: {
-  //     color: 'var(--error)',
-  //   },
-  //   focusInvalidField: true,
-  //   lockForm: true,
-
-  //   submitHandler: function(form) {
-  //     let formData = new FormData(form);
-  //     let xhr = new XMLHttpRequest();
-  //     xhr.onreadystatechange = function() {
-  //       if (xhr.readyState === 4) {
-  //         if (xhr.status === 200) {
-  //           console.log('Отправлено');
-  //         }
-  //       }
-  //     }
-  //     xhr.open('POST', 'mail.php', true);
-  //     xhr.send(formData);
-  //     form.reset();
-  //   }
-
-
-  // });
-
-  // validation
-  // .addField('.input-name', [
-  //   {
-  //     rule: 'minLength',
-  //     value: 3,
-  //     errorMessage: 'Имя должно содержать хотя бы 3 буквы'
-  //   },
-  //   {
-  //     rule: 'maxLength',
-  //     value: 30,
-  //     errorMessage: 'Имя не может содержать более 30 символов'
-  //   },
-  //   {
-  //     rule: 'required',
-  //     errorMessage: 'Вы не ввели имя'
-  //   }
-  // ])
-  // .addField('.input-tel', [
-  //   {
-  //     validator: () => {
-  //       const phone = phoneElement.inputmask.unmaskedvalue();
-  //       const result = Number(phone) && phone.length === 10;
-  //       return result === 0 ? false : result;
-  //     },
-  //     errorMessage: 'Вы не ввели телефон',
-  //   }
-  // ]);
 })();
